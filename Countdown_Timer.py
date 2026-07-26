d045adec944a69359771dddadd38fc36d8b7cccb
Dina Khaled Mamdouh <298262499+dinakhaled119@users.noreply.github.com>
2026-07-26 20:03:43 +0300

    Update countdown timer end message with new emoji

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -1,9 +1,13 @@
 import time
 
+print('======================')
+print('  Countdown Timer ⏱')
+print('======================')
+
 countdown = int(input('Seconds to countdown timer: '))
 
 for i in range(countdown,0,-1):
     print(f'\rTime remaining: {i} seconds', end='')
     time.sleep(1)
 
-print("\nTime's up! 🎉")
+print("\nTime's up! ⌛")
